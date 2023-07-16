3159db34a253c2c575ea46a393e45eee48b8b58f
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2023-07-16 17:34:43 +0800

    Fix block not consuming other slots

--- src/main/java/dev/effortless/building/operation/StructureBuildOperation.java
+++ src/main/java/dev/effortless/building/operation/StructureBuildOperation.java
@@ -6,6 +6,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -33,12 +34,7 @@ public final class StructureBuildOperation extends StructureOperation {
         this.storage = storage;
     }
 
-    private static BlockState getBlockStateFromMainHand(Player player, BlockHitResult hitResult) {
-        return getBlockStateFromItem(player, InteractionHand.MAIN_HAND, hitResult);
-    }
-
-    private static BlockState getBlockStateFromItem(Player player, InteractionHand hand, BlockHitResult hitResult) {
-        var itemStack = player.getItemInHand(hand);
+    private static BlockState getBlockStateFromItem(Player player, ItemStack itemStack, InteractionHand hand, BlockHitResult hitResult) {
         var blockPlaceContext = new BlockPlaceContext(player, hand, itemStack, hitResult);
         var item = itemStack.getItem();
 
@@ -56,11 +52,12 @@ public final class StructureBuildOperation extends StructureOperation {
                 return Stream.empty();
             }
             case PLACE_BLOCK -> {
+                var itemStack = player.getMainHandItem().copy();
                 return context.collect()
                         .map((hitResult) -> new SingleBlockPlaceOperation(
                                 level, player, context, storage,
                                 hitResult.getBlockPos(),
-                                getBlockStateFromMainHand(player, hitResult)))
+                                getBlockStateFromItem(player, itemStack, InteractionHand.MAIN_HAND, hitResult)))
                         .flatMap(Stream::of) // for modifiers
                         .map((op) -> op);
             }
